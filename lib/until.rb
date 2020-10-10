0af177c381e328aac729687f64f240185dff1ddd
Justin Movitz <movitzj1@montclair.edu>
2020-10-10 21:28:17 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,8 @@
 def using_until 
   levitation_force = 6
-  until levitation_force  == 10
-    
-    
+  until levitation_force == 10 do
+   levitation_force = levitation_force + 1
+   puts "Wingardium Leviosa"
+  end
 end
 
